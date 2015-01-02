7e4e8ee9c2bf502b3693980953eb01074da3da67
Chris Baptista <chris@asianchris.com>
2015-01-02 14:40:11 -0500

    Update Dev Server templates

--- script/packages/web.sh
+++ script/packages/web.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# Apache and PHP
-apt-get install -y apache2 php5 php5-mcrypt php5-gd php5-curl php5-mysql php5-memcached
+# Apache, PHP, curl, ruby
+apt-get install -y apache2 php5 php5-mcrypt php5-gd php5-curl php5-mysql php5-memcached curl ruby
 
 # Apache2 enable mod rewrite
 ln -s /etc/apache2/mods-available/rewrite.load /etc/apache2/mods-enabled/rewrite.load
@@ -9,3 +9,9 @@ ln -s /etc/apache2/mods-available/rewrite.load /etc/apache2/mods-enabled/rewrite
 # PHP5 enable MCRYPT
 ln -s /etc/php5/mods-available/mcrypt.ini /etc/php5/cli/conf.d/25-mcrypt.ini
 ln -s /etc/php5/mods-available/mcrypt.ini /etc/php5/apache2/conf.d/25-mcrypt.ini
+
+# Composer
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin --filename=composer
+
+# SASS
+gem install sass
