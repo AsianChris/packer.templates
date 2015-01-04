391f753098d32066811ab31eb1f5f075c19128ac
Chris Baptista <chris@asianchris.com>
2015-01-03 20:23:24 -0500

    Add base directory

--- script/packages/web.sh
+++ script/packages/web.sh
@@ -6,6 +6,9 @@ apt-get install -y apache2 php5 php5-mcrypt php5-gd php5-curl php5-mysql php5-me
 # Apache2 enable mod rewrite
 ln -s /etc/apache2/mods-available/rewrite.load /etc/apache2/mods-enabled/rewrite.load
 
+# Create directories
+mkdir -p /mnt/httpd/
+
 # PHP5 enable MCRYPT
 ln -s /etc/php5/mods-available/mcrypt.ini /etc/php5/cli/conf.d/25-mcrypt.ini
 ln -s /etc/php5/mods-available/mcrypt.ini /etc/php5/apache2/conf.d/25-mcrypt.ini
